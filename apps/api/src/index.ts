eb86a9c62d3df4fc2eb369a27a90996d49105663
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-08-14 13:56:52 +0530

    initialised backend

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import { Hono} from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+    return c.json({
+        status: "ok",
+    });
+});
+
+export default {
+    port: 8080,
+    fetch: app.fetch,
+};
