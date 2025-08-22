86af4f89a121cb63cbd3abb2e14fd95fd48a39a3
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-08-22 18:05:08 +0530

    added api keys plugin

--- apps/console/app/lib/auth.ts
+++ apps/console/app/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
+import { apiKey } from "better-auth/plugins";
 import { prisma } from "db";
 
 export const auth = betterAuth({
@@ -12,5 +13,17 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         },
     },
-    plugins: [nextCookies()],
+    plugins: [
+        nextCookies(),
+        apiKey({
+            apiKeyHeaders: ['azen-api-key'],
+            enableMetadata: true,
+            permissions: {
+                defaultPermissions: {
+                    file: ['read'],
+                    projects: ['read'],
+                },
+            },
+        }),
+    ], 
 });
