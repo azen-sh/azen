a38693481d4d0de90dc8d750e5c16bd6539a0866
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-09-12 19:12:35 +0530

    added chunk file

---
+++ apps/api/src/lib/chunk.ts
@@ -0,0 +1,14 @@
+import { encodingForModel } from 'js-tiktoken';
+
+const enc = encodingForModel('gpt-4o');
+
+export function chunkText(text: string, maxTokens = 512, overlap = 50) {
+    const tokens = enc.encode(text);
+    const chunks: string[] = [];
+    for(let i = 0; i < tokens.length; i += (maxTokens - overlap)) {
+        const sliced = tokens.slice(i, i + maxTokens);
+        chunks.push(enc.decode(sliced));
+    };
+    console.log(chunks);
+    return chunks;
+}; 
--- apps/api/src/routes/memory.ts
+++ apps/api/src/routes/memory.ts
@@ -1,11 +1,15 @@
 import { Hono} from "hono";
 import { z } from "zod";
+import { chunkText } from "../lib/chunk";
 
 const router = new Hono();
 
-router.get("/", async (c) => {
+router.post("/", async (c) => {
     const userId = c.get('userId');
     const apiKeyId = c.get('apiKeyId');
+
+    const body = await c.req.json();
+    chunkText(body.text);
     return c.json({
         status: `all good, userId: ${userId}, apiKeyId: ${apiKeyId}`,
     });
