99ab2d2e30cfef3e218d5acebcff5013872b16ab
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-12-01 23:59:35 +0530

    .

--- apps/api/src/middlewares/authMiddleware.ts
+++ apps/api/src/middlewares/authMiddleware.ts
@@ -2,7 +2,9 @@ import { createMiddleware } from "hono/factory";
 import { auth } from "../lib/auth";
 
 export const authMiddleware = createMiddleware(async (c, next) => {
+    console.log(c.req.header());
     const session = await auth.api.getSession({ headers: c.req.header() });
+    console.log(session);
     
     if (!session || !session.user) {
         return c.json({ error: 'Unauthorized' }, 401);
