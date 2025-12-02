ecced7265c10e6264910c93455825277157a2288
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-12-02 13:22:37 +0530

    fix auth middleware

--- apps/api/src/middlewares/authMiddleware.ts
+++ apps/api/src/middlewares/authMiddleware.ts
@@ -1,13 +1,8 @@
 import { createMiddleware } from "hono/factory";
 import { auth } from "../lib/auth";
-import { getCookie } from "hono/cookie";
 
 export const authMiddleware = createMiddleware(async (c, next) => {
-    console.log("headers",c.req.raw.headers);
-    const sessionToken = getCookie(c, 'better-auth.session_token');
-    console.log("sessionToken",sessionToken);
-    const session = await auth.api.getSession({ headers: c.req.header() });
-    console.log(session);
+    const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
     if (!session || !session.user) {
         return c.json({ error: 'Unauthorized' }, 401);
