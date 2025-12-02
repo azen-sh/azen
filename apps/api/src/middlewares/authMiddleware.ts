1b9ce14db804d6eef965242f6cf26a55c6c28b53
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-12-02 12:54:55 +0530

    .

--- apps/api/src/middlewares/authMiddleware.ts
+++ apps/api/src/middlewares/authMiddleware.ts
@@ -1,11 +1,14 @@
 import { createMiddleware } from "hono/factory";
 import { auth } from "../lib/auth";
+import { getCookie } from "hono/cookie";
 
 export const authMiddleware = createMiddleware(async (c, next) => {
-    console.log(c.req.header());
+    console.log("headers",c.req.raw.headers);
+    const sessionToken = getCookie(c, 'better-auth.session_token');
+    console.log("sessionToken",sessionToken);
     const session = await auth.api.getSession({ headers: c.req.header() });
     console.log(session);
-    
+
     if (!session || !session.user) {
         return c.json({ error: 'Unauthorized' }, 401);
     };
