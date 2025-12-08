5e48e509cad2764c1a61d64a1f0afaea5ad0f73d
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-12-09 02:53:18 +0530

    added sitemap for landing

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,21 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://azen.sh",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://azen.sh/about",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://azen.sh/terms",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://azen.sh/privacy",
+        lastModified: new Date(),
+      },
+    ]
+  }
+  
